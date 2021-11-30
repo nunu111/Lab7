863a89ef181286c9e587da67e22882bbeca876d1
nunu111 <nunu111za@hotmail.com>
2021-11-30 15:56:10 +0700

    lab7_2 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,36 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
+	int i = 0, P[5000],k;
+	while(i < 5000) //array p
+	{
 		P[i] = 2*i+1;
 		i++;
 	}
-	
+	cout << "Please input k: ";
+	cin >> k;
+	if(k>0)
+	{
+		int i=0,result=0;
+		while(i <5000)
+		{
+			if(P[i]%k == 0)
+			{
+				result = result - P[i];
+				i++;
+			}
+			else
+			{
+				result = result + P[i];
+				i++;
+			}
+		}
+		cout << "Result = " <<result;
+	}
+	else
+	{
+		cout << "Invalid input!!!";
+	}
+
 	return 0;
 }
