d330d5568227bc7e2a8dd3fb1abe567f0032493e
nunu111 <nunu111za@hotmail.com>
2021-11-30 17:59:08 +0700

    lab7_3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,65 +1,19 @@
 #include<iostream>
-
 using namespace std;
+
 int adiff(int x,int y)
 {
-  while(x >= 360)
-  {
-    x = x-360;
-  }
-  while(y >= 360)
-  {
-    y = y-360;
-  }
-  if(x <0)//x -
-  {
-    if(y<0)//y -
-    {
-      if(x<y)
-      {
-        return y-x;
-      }
-      if(y<x)
-      {
-        return x-y;
-      }
-    }
-    if(y>=0)//y+
-    {
-      return (x*-1)+y;
-    }
-  }
-  if(x >= 0)//x+
-  {
-    if(y<0)//y-
-    {
-      return x+(y*-1);
-    }
-    if(y>=0)//y+
-    {
-      }
-      if(x>y)
-      {
-        return x-y;
-        if(x-y>180)
-        {
-          return 360-(x-y);
-        }
-      }
-      if(y>x)
-      {
-        if(y-x>180)
-        {
-          return 360-(y-x);
-        }
-        return y-x;
-      }
-    }
-  if(x==y)
-      {
-        return 0;
-      }
-      return 0;
+int z;
+z = x - y;
+if(z<0)
+{
+  z=z*(-1);
+}
+z = z%360;
+if(z > 180){
+    return 360-z;
+}
+return z;
 }
 
 int main(){
